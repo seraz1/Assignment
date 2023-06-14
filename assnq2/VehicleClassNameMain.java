4216b99e179d1dcd375d73259d02f28e05a1b510
Shabbu1 <alamseraz306@gmail.com>
2023-06-13 21:54:00 -0700

    first commit

---
+++ assnq2/VehicleClassNameMain.java
@@ -0,0 +1,12 @@
+package assnq2;
+
+public class VehicleClassNameMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Vehicleclass myObj =new Vehicleclass();
+		
+	}
+
+}
---
+++ assnq2/Vehicleclass.java
@@ -0,0 +1,8 @@
+package assnq2;
+
+public class Vehicleclass {
+
+	String x = "VehicleName";
+	String y = "numberOfGear";
+	
+}
